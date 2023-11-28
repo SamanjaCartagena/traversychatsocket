df4d20ab6d333b4471e6b40c202df633ddd63d38
SamanjaCartagena <cart.samanja09@gmail.com>
2023-11-28 17:47:18 -0500

    rooms built to join

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,46 @@
 const chatForm=document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+
+//Get username and room from URL
+const {username,room}=Qs.parse(location.search,{
+    ignoreQueryPrefix:true
+});
 
 const socket = io();
-
-
+//Join chatroom
+socket.emit(('joinRoom',{username,room}))
+//Message from server
 socket.on('message',message =>{
     console.log(message)
+    outputMessage(message)
+
+    //Scroll down
+chatMessages.scrollTop= chatMessages.scrollHeight
 })
 
 
 chatForm.addEventListener('submit',(e) =>{
   e.preventDefault();
 
+  //Get message text
   const msg= e.target.elements.msg.value
 
-  console.log(msg);
+  //Emit message to server
+  socket.emit('chatMessage',msg);
+
+  //Clear input
+  e.target.elements.msg.value=''
 })
+
+//Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div')
+    div.classList.add('message');
+    div.innerHTML=`<p class="meta">${message.username}
+    <span>${message.time}</span>
+    <p class="text">${message.text}
+    </p>`;
+
+    document.querySelector('.chat-messages').appendChild(div);
+
+}
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path= require('path');
 const http= require('http')
 const express = require('express')
 const {Server} = require('socket.io')
+const formatMessage = require('./utils/messages')
+const {userJoin,getCurrentUser}= require('./utils/users');
+
 const app = express()
 
 const cors = require('cors')
@@ -9,6 +12,9 @@ const server=http.createServer(app)
 app.use(cors())
 //Set static folder
 app.use(express.static(path.join(__dirname,'public')));
+
+const botName ='ChatCord Bot'
+
 const io = new Server(server, {
     cors:{
         origin:"http://localhost:3000",
@@ -16,16 +22,26 @@ const io = new Server(server, {
     }
 });
 io.on('connection',socket =>{
-    socket.emit('message','Welcome to ChatCord!')
-    io.emit('message','A user has joined the chat');
 
-    //Runs when client disconnects
-    socket.on('disconnect',()=>{
+    socket.on('joinRoom',({username, room})=>{
+        const user = userJoin(socket.id,username,room);
+        socket.join(user.room)
+ socket.on('disconnect',()=>{
       io.emit('message','A user has left the chat')
     });
 
-    //Broadcast when a user connects
+     socket.on('chatMessage',(msg) =>{
+        io.emit('message',formatMessage('USER',msg))
+     })
+
+    })
+    socket.emit('message',formatMessage(botName,'Welcome to ChatCord!'))
+    io.emit('message','A user has joined the chat');
+
+    //Runs when client disconnects
    
+     
+    
 })
 
 const PORT = 3000 || process.env.PORT;
